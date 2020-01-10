eeb949b1848eaedffd73c82bfdfbd103f4e8c8f6
Андрей <ande5@mail.ru>
2020-01-10 20:32:55 +0300

    Added NeuralNetworkLib and NNTest projects

---
+++ NNTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NeuralNetworkLib;
+
+namespace NNTest
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceNN = new ServiceNN(100000);
+
+            //serviceNN.Train();
+            var result = serviceNN.Handle(new double[] {50, 50, 50});
+            Console.ReadKey();
+        }
+    }
+}
