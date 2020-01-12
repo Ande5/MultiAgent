9dc1bea45e8ef4b7d9884e6b9dc2730bf0d493c5
Turchak411 <t-a-a-96@mail.ru>
2020-01-12 19:02:58 +0300

    Added loading map

--- MultiAgentSystem/ServiceManager/FileManager.cs
+++ MultiAgentSystem/ServiceManager/FileManager.cs
@@ -33,5 +33,30 @@ namespace MultiAgentSystem.ServiceManager
                 }
             }
         }
+
+        public int[,] LoadMap()
+        {
+            int[,] mapDepths;
+
+            using (StreamReader fileReader = new StreamReader(_path))
+            {
+                int sizeMapX = Convert.ToInt32(fileReader.ReadLine());
+                int sizeMapY = Convert.ToInt32(fileReader.ReadLine());
+
+                mapDepths = new Int32[sizeMapX, sizeMapY];
+
+                for (int i = 0; i < mapDepths.GetLength(0); i++)
+                {
+                    for (int k = 0; k < mapDepths.GetLength(1); k++)
+                    {
+                        mapDepths[i, k] = Convert.ToInt32(fileReader.ReadLine());
+                    }
+
+                    fileReader.ReadLine();
+                }
+            }
+
+            return mapDepths;
+        }
     }
 }
