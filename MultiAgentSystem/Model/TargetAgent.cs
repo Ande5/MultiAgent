000f505a7e347584a581257c9bdbd2c157cd5e51
Андрей <ande5@mail.ru>
2020-01-16 14:03:57 +0300

    Added method AddShipUI and method AddTargetUI

--- MultiAgentSystem/Model/TargetAgent.cs
+++ MultiAgentSystem/Model/TargetAgent.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MultiAgentSystem.Model
+﻿namespace MultiAgentSystem.Model
 {
-    public class TargetAgent: Agent
-    {
-    }
+    public class TargetAgent: Agent { }
 }
